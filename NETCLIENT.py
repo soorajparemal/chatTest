4449e210ddbf865db7702ccb4d78984f6c27110e
sooraj <srjps202@gmail.com>
2016-07-20 18:09:22 +0530

    chat

---
+++ NETCLIENT.py
@@ -0,0 +1,5 @@
+from socket import *
+text=raw_input('Enter your message : ')
+fd=socket(AF_INET,SOCK_DGRAM)
+fd.sendto(str(text),('127.0.0.1',7000))
+
---
+++ NETSERVER.py
@@ -0,0 +1,5 @@
+from socket import *
+fd=socket(AF_INET,SOCK_DGRAM)
+fd.bind(('127.0.0.1',7000))
+text=fd.recvfrom(100)
+print text
